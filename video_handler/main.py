dbf71979922d5a24e92c4e7f0566c68f08ba7bdf
Choi-syoon <neulet.git@gmail.c0om>
2024-09-24 12:14:05 +0900

    코드 정리

--- video_handler/main.py
+++ video_handler/main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-import random
 import uuid
+import random
+from pydantic import BaseModel
+from datetime import datetime, timezone
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-# 비디오 메타데이터 모델 정의
 class VideoMetadata(BaseModel):
     url: str
     title: str
@@ -13,39 +13,35 @@ class VideoMetadata(BaseModel):
     width: int
     height: int
     fps: int
+    created_at: datetime
 
-# 요청 데이터 모델 정의
 class TextRequest(BaseModel):
     text: str
 
-# 비디오 메타데이터 생성 함수
-def generate_video_metadata(text: str) -> VideoMetadata:
-    # 예시로 텍스트 길이에 따라 비디오 메타데이터를 생성
+def dummy_metadata(text: str) -> VideoMetadata:
     random_width = random.randint(480, 1920)
     random_height = random.randint(320, 1080)
     random_fps = random.choice([24, 30, 60])
-    random_total_time = random.uniform(10.0, 300.0)  # 10초에서 300초 사이의 비디오 길이
+    total_time = random.uniform(1.0, 300.0)
+    create_time = datetime.now(timezone.utc)
 
     return VideoMetadata(
-        url=f"http://example.com/videos/{uuid.uuid4()}.mp4",  # 랜덤한 비디오 URL 생성
-        title=f"Video for {text[:10]}",  # 텍스트의 앞부분을 제목으로 사용
-        total_time=random_total_time,
+        url=f"http://example.com/videos/{uuid.uuid4()}.mp4",  
+        title=f"Video for {text[:10]}",
+        total_time=total_time,  
         width=random_width,
         height=random_height,
-        fps=random_fps
+        fps=random_fps,  
+        created_at=create_time
     )
 
 @app.post("/")
 async def receive_text(text_request: TextRequest):
     try:
-        # 클라이언트로부터 받은 텍스트
         text = text_request.text
         print(f"Received text: {text}")
+        video_metadata = dummy_metadata(text)
 
-        # 비디오 메타데이터 생성
-        video_metadata = generate_video_metadata(text)
-
-        # 응답으로 비디오 메타데이터 반환
-        return video_metadata
+        return video_metadata.dict()  
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
