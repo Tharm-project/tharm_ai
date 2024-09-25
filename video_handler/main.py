82247673e7f16c2d30b478dcb64a9b5d392912e8
Choi-syoon <neulet.git@gmail.com>
2024-09-26 02:02:47 +0900

    [ADD] Firebase 설정 & 데모 영상 buket 업로드 테스트

--- video_handler/main.py
+++ video_handler/main.py
@@ -3,45 +3,26 @@ import random
 from pydantic import BaseModel
 from datetime import datetime, timezone
 from fastapi import FastAPI, HTTPException
+from firebase_controller import Controller
 
 app = FastAPI()
 
 class VideoMetadata(BaseModel):
     url: str
-    title: str
-    total_time: float
-    width: int
-    height: int
-    fps: int
-    created_at: datetime
 
 class TextRequest(BaseModel):
     text: str
 
-def dummy_metadata(text: str) -> VideoMetadata:
-    random_width = random.randint(480, 1920)
-    random_height = random.randint(320, 1080)
-    random_fps = random.choice([24, 30, 60])
-    total_time = random.uniform(1.0, 300.0)
-    create_time = datetime.now(timezone.utc)
-
-    return VideoMetadata(
-        url=f"http://example.com/videos/{uuid.uuid4()}.mp4",  
-        title=f"Video for {text[:10]}",
-        total_time=total_time,  
-        width=random_width,
-        height=random_height,
-        fps=random_fps,  
-        created_at=create_time
-    )
 
 @app.post("/")
 async def receive_text(text_request: TextRequest):
     try:
         text = text_request.text
         print(f"Received text: {text}")
-        video_metadata = dummy_metadata(text)
+        local_file_path = "./video_handler/1m_demo.mp4"  # Example local video file path
+        storage_file_name = f"videos/{uuid.uuid4()}.mp4"  # Unique filename for Firebase
+        uploaded_url = Controller.upload_video(local_file_path, storage_file_name)
+        return {"video_url": uploaded_url}
 
-        return video_metadata.dict()  
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
